4208b801e6c5267083b1962864ab396c8146278f
furube.seigi <furube.seigi@tpj.co.jp>
2025-11-19 21:27:57 +0900

    first_commit

---
+++ configuration.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal-combined.nix> ];
+
+  services.openssh = {
+      enable = true;
+      ports = [22];
+  };
+
+  users.users.frse = {
+      isNomalUser = true;
+      extraGroups = ["wheel"];
+      hashedPassword = "$6$eq8vfHcH9q.1xDev$Y2aKyNEgTxMTbxEMPy1qe7Od/gorzLVSU1fA1pCAj4c38SeQVPAdL8fwN1sk5ryuSWR.NcJA9el4JmoHP2yxS1";
+  };
+}
