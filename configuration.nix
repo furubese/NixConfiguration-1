870cd2cafbf12d0ce1a7b98b869d7ef4cb3982bd
furube.seigi <furube.seigi@tpj.co.jp>
2025-11-19 22:39:13 +0900

    nix settings

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,8 @@
   # Legacy
   # imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal-combined.nix> ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   services.openssh = {
       enable = true;
       ports = [22];
@@ -14,5 +16,7 @@
       extraGroups = ["wheel"];
       hashedPassword = "$6$eq8vfHcH9q.1xDev$Y2aKyNEgTxMTbxEMPy1qe7Od/gorzLVSU1fA1pCAj4c38SeQVPAdL8fwN1sk5ryuSWR.NcJA9el4JmoHP2yxS1";
   };
+
+  system.stateVersion = "25.05";
 
 }
