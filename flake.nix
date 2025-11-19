2d1882e7a34b7523879835c0ffd8c15b61d431b8
furube.seigi <furube.seigi@tpj.co.jp>
2025-11-19 22:15:07 +0900

    hm?

--- flake.nix
+++ flake.nix
@@ -6,13 +6,12 @@
     # NixOS official package source
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     k0s-nix.url = "github:johbo/k0s-nix";
-    system = "x86_64-linux";
   };
 
-  outputs = { self, nixpkgs, k0s-nix, ... } inputs:
+  outputs = { self, nixpkgs, k0s-nix, ... }@inputs:
   {
      nixosConfigurations.controller = nixpkgs.lib.nixosSystem {
-       inherit system;
+       system = "x86_64-linux";
        modules = [
           ./configuration.nix
           k0s-nix.nixosModules.default
